01621c1a4b404c6670ab4196ef68e55f878da4d8
Asus <gurlukgoksel@gmail.com>
2024-07-06 18:06:11 +0300

    tailwindCssProje

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          light: '#D6BCFA',
+          DEFAULT: '#9F7AEA',
+          dark: '#6B46C1',
+        },
+        width: {
+          'custom-width': '950px',
+        },
+        height: {
+          'custom-height': '330px',
+        },
+      },
+    },
+    
+  },
+  plugins: [],
+}
+
